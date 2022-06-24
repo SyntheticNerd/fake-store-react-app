99876e8831f216e9aebc1eb4c2a9a2cfc1b4483d
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-06-24 12:43:48 -0700

    added link to fake store api:

--- src/components/products/Catagories.js
+++ src/components/products/Catagories.js
@@ -18,21 +18,23 @@ class Catagories extends React.Component {
         "https://fakestoreapi.com/img/81Zt42ioCgL._AC_SX679_.jpg",
         "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
         "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
-        "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
+        "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg",
       ],
-      error: false
+      error: false,
     };
   }
   componentDidMount() {
-    getProducts("products/categories").then((res) => {
-      if(!res.ok){
-        throw new Error("HTTP error " + res.status);
-      }
-      this.setState({ categories: res });
-    }).catch(error=>{
-      console.log(error.message)
-      this.setState({error: true})
-    });
+    getProducts("products/categories")
+      .then((res) => {
+        if (!res.ok) {
+          throw new Error("HTTP error " + res.status);
+        }
+        this.setState({ categories: res });
+      })
+      .catch((error) => {
+        console.log(error.message);
+        this.setState({ error: true });
+      });
   }
   getRandomInt(max) {
     return Math.floor(Math.random() * max);
@@ -50,17 +52,40 @@ class Catagories extends React.Component {
           style={{
             display: "flex",
             flexWrap: "wrap",
-            justifyContent: "center"
+            justifyContent: "center",
           }}
         >
-          {!this.state.error ? this.state.categories.map((category, index) => (
-            <div key={`${index}${category}`}>
-              <CatBtn onClick={() => this.onClick(category)}>
-                <Thumbnail url={this.state.images[index]} />
-                {category}
-              </CatBtn>
-            </div>
-          )) : <h1 style={{marginTop: "80px", color: "white", backgroundColor: "#444444", boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.5)", padding: "32px", borderRadius: "8px"}}>Cannot contact Fake Store API</h1>}
+          {!this.state.error ? (
+            this.state.categories.map((category, index) => (
+              <div key={`${index}${category}`}>
+                <CatBtn onClick={() => this.onClick(category)}>
+                  <Thumbnail url={this.state.images[index]} />
+                  {category}
+                </CatBtn>
+              </div>
+            ))
+          ) : (
+            <h1
+              style={{
+                marginTop: "80px",
+                color: "white",
+                backgroundColor: "#444444",
+                boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.5)",
+                padding: "32px",
+                borderRadius: "8px",
+              }}
+            >
+              Cannot contact Fake Store API <br />{" "}
+              <a
+                href='https://fakestoreapi.com/'
+                style={{ color: "pink" }}
+                target='_blank'
+                rel='noreferrer noopener'
+              >
+                https://fakestoreapi.com/
+              </a>
+            </h1>
+          )}
         </div>
       </>
     );
