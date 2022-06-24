1dfff22f6f1618566dd2f7a36c3f1d0cef5b5a8a
SyntheticDesigner <andrew.sch.j@gmail.com>
2022-06-24 12:40:17 -0700

    added api error handling

--- src/components/products/Catagories.js
+++ src/components/products/Catagories.js
@@ -19,12 +19,19 @@ class Catagories extends React.Component {
         "https://fakestoreapi.com/img/71YAIFU48IL._AC_UL640_QL65_ML3_.jpg",
         "https://fakestoreapi.com/img/71li-ujtlUL._AC_UX679_.jpg",
         "https://fakestoreapi.com/img/61pHAEJ4NML._AC_UX679_.jpg"
-      ]
+      ],
+      error: false
     };
   }
   componentDidMount() {
     getProducts("products/categories").then((res) => {
+      if(!res.ok){
+        throw new Error("HTTP error " + res.status);
+      }
       this.setState({ categories: res });
+    }).catch(error=>{
+      console.log(error.message)
+      this.setState({error: true})
     });
   }
   getRandomInt(max) {
@@ -46,14 +53,14 @@ class Catagories extends React.Component {
             justifyContent: "center"
           }}
         >
-          {this.state.categories.map((category, index) => (
+          {!this.state.error ? this.state.categories.map((category, index) => (
             <div key={`${index}${category}`}>
               <CatBtn onClick={() => this.onClick(category)}>
                 <Thumbnail url={this.state.images[index]} />
                 {category}
               </CatBtn>
             </div>
-          ))}
+          )) : <h1 style={{marginTop: "80px", color: "white", backgroundColor: "#444444", boxShadow: "0px 8px 16px rgba(0, 0, 0, 0.5)", padding: "32px", borderRadius: "8px"}}>Cannot contact Fake Store API</h1>}
         </div>
       </>
     );
